68038c72e613a5a7553510b32ab2d38e736e2284
Mansour Behabadi <mansour@oxplot.com>
2023-02-06 19:57:15 +1100

    Implement CLI query

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"sort"
 	"strings"
 
 	_ "github.com/oxplot/starenv/autoload"
@@ -21,6 +22,7 @@ var (
 	clickupAPIToken = os.Getenv("CLICKUP_API_KEY")
 
 	indexFolderFlag = flag.Bool("index", false, "Index file paths passed on stdin")
+	cliQuery        = flag.String("query", "", "Run in command line mode")
 )
 
 // Vector is a vector of floats.
@@ -101,12 +103,13 @@ func runIndex() error {
 		if err != nil {
 			return err
 		}
-		defer f.Close()
 
 		doc := Document{}
 		if err := json.NewDecoder(f).Decode(&doc); err != nil {
+			f.Close()
 			return err
 		}
+		f.Close()
 
 		// For each chunk, get the embedding.
 
@@ -149,12 +152,12 @@ func runIndex() error {
 	if err != nil {
 		return err
 	}
-	defer f.Close()
 	gz := gzip.NewWriter(f)
+	defer gz.Close()
 	if err := json.NewEncoder(gz).Encode(idx); err != nil {
 		return err
 	}
-	return f.Sync()
+	return gz.Flush()
 }
 
 func runServer() error {
@@ -169,14 +172,107 @@ func runServer() error {
 	return nil
 }
 
+func loadIndex() (idx Index, err error) {
+	f, err := os.Open("index.json.gz")
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	gz, err := gzip.NewReader(f)
+	if err != nil {
+		return
+	}
+	err = json.NewDecoder(gz).Decode(&idx)
+	return
+}
+
+func runCLI(query string) error {
+
+	// Load the index
+
+	idx, err := loadIndex()
+	if err != nil {
+		return fmt.Errorf("failed to load index: %w", err)
+	}
+
+	// Get the embedding for the query
+
+	ctx := context.Background()
+	aiCl := gogpt.NewClient(openAIAuthToken)
+	resp, err := aiCl.CreateEmbeddings(ctx, gogpt.EmbeddingRequest{
+		Input: []string{query},
+		Model: gogpt.AdaEmbeddingV2,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to get embedding for query: %w", err)
+	}
+	qEmb := resp.Data[0].Embedding
+
+	// Sort the documents by closest distance to the query
+
+	sort.Slice(idx.Embeddings, func(i, j int) bool {
+		return idx.Embeddings[i].Vector.Distance(qEmb) < idx.Embeddings[j].Vector.Distance(qEmb)
+	})
+
+	// Join the chunk content of the first 3 chunks.
+
+	var chunks []string
+	for i := 0; i < 3; i++ {
+		chunkRef := idx.ChunkRefs[idx.Embeddings[i].ChunkID]
+		docPath := idx.Documents[chunkRef.DocumentID]
+		f, err := os.Open(docPath)
+		if err != nil {
+			return err
+		}
+		var doc Document
+		if err := json.NewDecoder(f).Decode(&doc); err != nil {
+			f.Close()
+			return err
+		}
+		f.Close()
+		chunks = append(chunks, doc.Chunks()[chunkRef.ChunkNumber])
+	}
+
+	// Create the prompt
+
+	prompt := `Answer the question as truthfully as possible using the provided text, and if the answer is not contained within the text below, say "I don't know"\n\n`
+	prompt += fmt.Sprintf("Context: %s\n\n", strings.Join(chunks, " "))
+	prompt += fmt.Sprintf("Q: %s\nA:", query)
+
+	// Generate the answer
+
+	compResp, err := aiCl.CreateCompletion(ctx, gogpt.CompletionRequest{
+		Prompt:           prompt,
+		Model:            gogpt.GPT3TextDavinci003,
+		MaxTokens:        300,
+		TopP:             1,
+		FrequencyPenalty: 0,
+		Temperature:      0,
+		PresencePenalty:  0,
+	})
+	if err != nil {
+		return fmt.Errorf("completion failed: %w", err)
+	}
+
+	fmt.Printf("Answer: %s\n", compResp.Choices[0].Text)
+	return nil
+}
+
 func main() {
 	log.SetFlags(0)
 	flag.Parse()
+
 	if *indexFolderFlag {
 		if err := runIndex(); err != nil {
 			log.Fatal(err)
 		}
 		return
+	}
+
+	if *cliQuery != "" {
+		if err := runCLI(*cliQuery); err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	if err := runServer(); err != nil {
